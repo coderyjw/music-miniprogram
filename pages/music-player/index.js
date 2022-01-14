c6f5f66a57cb6cc6f37b52bcdc4f3df7106628e5
叶继伟 <729721503@qq.com>
2022-01-15 06:36:39 +0800

    feat: 播放页面布局  css

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -9,6 +9,9 @@ Page({
   data: {
     id: 0,
     currentSong: {},
+
+    currentPage: 0,
+    contentHeight: 0
   },
 
   /**
@@ -21,6 +24,20 @@ Page({
 
     // 2.根据id获取歌曲信息
     this.getPageData(id)
+
+     // 3.动态计算内容高度
+     const globalData = getApp().globalData
+     const screenHeight = globalData.screenHeight
+     const statusBarHeight = globalData.statusBarHeight
+     const navBarHeight = globalData.navBarHeight
+     const contentHeight = screenHeight - statusBarHeight - navBarHeight
+     console.log({screenHeight,statusBarHeight,navBarHeight,contentHeight})
+     this.setData({ contentHeight })
+
+      // 4.创建播放器
+    const audioContext = wx.createInnerAudioContext()
+    audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
+    audioContext.play()
   },
 
   // 网络请求
@@ -28,5 +45,11 @@ Page({
     getSongDetail(id).then(res => {
       this.setData({ currentSong: res.songs[0] })
     })
+  },
+
+  // 事件处理
+  handleSwiperChange: function(event) {
+    const current = event.detail.current
+    this.setData({ currentPage: current })
   },
 })
