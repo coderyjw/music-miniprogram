3d848c317d0cc1abe11156c15e384c3001cd2ba3
叶继伟 <729721503@qq.com>
2022-01-15 19:43:51 +0800

    feat: 播放滚动条添加事件监听

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -10,11 +10,14 @@ Page({
   data: {
     id: 0,
     currentSong: {},
+    currentTime: 0,
     durationTime: 0,
 
     currentPage: 0,
     contentHeight: 0,
+    sliderValue: 0,
     isMusicLyric: true,
+    isSliderChanging: false
   },
 
   /**
@@ -41,6 +44,9 @@ Page({
      audioContext.stop()
      audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
      audioContext.autoplay = true
+
+     // 5.audioContext的事件监听
+    this.setupAudioContextListener()
   },
 
   // 网络请求
@@ -50,9 +56,65 @@ Page({
     })
   },
 
+  setupAudioContextListener: function() {
+    audioContext.onCanplay(() => {
+      audioContext.play()
+    })
+
+    // 监听时间改变
+    audioContext.onTimeUpdate(() => {
+      // 1.获取当前时间
+      const currentTime = audioContext.currentTime * 1000
+
+      // 2.根据当前时间修改currentTime/sliderValue
+      if (!this.data.isSliderChanging) {
+        const sliderValue = currentTime / this.data.durationTime * 100
+        this.setData({ sliderValue, currentTime })
+      }
+
+      // // 3.根据当前时间去查找播放的歌词
+      // let i = 0
+      // for (; i < this.data.lyricInfos.length; i++) {
+      //   const lyricInfo = this.data.lyricInfos[i]
+      //   if (currentTime < lyricInfo.time) {
+      //     break
+      //   }
+      // }
+      // // 设置当前歌词的索引和内容
+      // const currentIndex = i - 1
+      // if (this.data.currentLyricIndex !== currentIndex) {
+      //   const currentLyricInfo = this.data.lyricInfos[currentIndex]
+      //   this.setData({ currentLyricText: currentLyricInfo.text, currentLyricIndex: currentIndex })
+      // }
+    })
+  },
+
   // 事件处理
   handleSwiperChange: function(event) {
     const current = event.detail.current
     this.setData({ currentPage: current })
   },
+
+  handleSliderChanging: function(event) {
+    const value = event.detail.value
+    const currentTime = this.data.durationTime * value / 100
+    this.setData({ isSliderChanging: true, currentTime, sliderValue: value })
+  },
+
+  handleSliderChange: function(event) {
+    // 1.获取slider变化的值
+    const value = event.detail.value
+
+    // 2.计算需要播放的currentTIme
+    const currentTime = this.data.durationTime * value / 100
+
+    // 3.设置context播放currentTime位置的音乐
+    audioContext.pause()
+    audioContext.seek(currentTime / 1000)
+
+    // 4.记录最新的sliderValue, 并且需要讲isSliderChaning设置回false
+    this.setData({ sliderValue: value, isSliderChanging: false })
+  },
+
+
 })
